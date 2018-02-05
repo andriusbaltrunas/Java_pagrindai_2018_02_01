cb02644a793693a0533f36e2935325f36a1b55ba
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-02-05 19:14:50 +0200

    try/catch

--- src/ThirdExample.java
+++ src/ThirdExample.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -8,13 +9,14 @@ public class ThirdExample {
     public static void main(String[] args) {
         System.out.println("Pasirinkite punkta\n1. Trikimpis\n2. Staciakampis\n3. Kvadratas\n4. Apskritimas");
         Scanner sc = new Scanner(System.in);
-        int action = sc.nextInt();
+        int action = getCorrectNumber(sc);
+
         switch (action){
             case 1:
                 System.out.println("Iveskite krasteine a");
-                int petras = sc.nextInt();
+                int petras = getCorrectNumber(sc);
                 System.out.println("Iveskite krasteine b");
-                int antanas = sc.nextInt();
+                int antanas = getCorrectNumber(sc);
                 double result = trikampis(petras, antanas);
                 System.out.println("Trikampio plotas yra " + result);
                 break;
@@ -32,5 +34,21 @@ public class ThirdExample {
 
     private static double trikampis(int a, int b){
         return (a*b)/2;
+    }
+
+    private static int getCorrectNumber(Scanner sc){
+        int numb = 0;
+        while (true){
+            System.out.println("Iveskite skaicius");
+            try {
+                numb = sc.nextInt();
+                break;
+            }catch (InputMismatchException e){
+                System.out.println("Juk sakiau kad ivesk skaiciu!!!!");
+                sc.nextLine();
+            }
+        }
+
+        return numb;
     }
 }
